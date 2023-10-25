44fe1dfae50914c5fea8c3ba971ee49c5e844d71
Ashitaka-Laputa-I <Ashitaka-Laputa-I@outlook.com>
2023-10-25 08:14:33 +0800

    增加Setting类,由此修改game文件

--- game.py
+++ game.py
@@ -3,17 +3,25 @@
 import sys
 import pygame
 
+from setting import Setting
+
 
 def run_game():
 	pygame.init()
-	screen = pygame.display.set_mode((1920, 1080))
-	pygame.display.set_caption('Alien Invasion')
+
+	game_setting = Setting()
+	screen = pygame.display.set_mode((game_setting.screen_width, game_setting.screen_height))
+	pygame.display.set_caption(game_setting.caption)
 
 	while True:
 	 	# 监视键盘和鼠标事件
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: 
 	   			sys.exit() 
+	   			
+		# 每次循环时都重绘屏幕 
+		screen.fill(game_setting.bg_color)
+
 	 	# 让最近绘制的屏幕可见
 		pygame.display.flip() 
 
