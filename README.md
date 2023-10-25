06911225f736346a1bbe17988a38a00511a887ef
Ashitaka-Laputa-I <Ashitaka-Laputa-I@outlook.com>
2023-10-25 08:58:23 +0800

    增添ship并且绘制,修改setting的屏幕大小

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# Setting
+# Ship
 
-- 增加setting类
+- 新增Ship类
+  - 增加init方法
+  - 增加biltme方法
 
 
 
 # Game
 
-- 把setting模块中的Setting类导入,并由此修改程序
+- 增加ship属性
+- 绘制ship对象
+
+
+
+# Setting
+
+- 修改屏幕大小,以适合飞船大小(自己画的xD,只能用屏幕适应图片了)
--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import sys
 import pygame
 
 from setting import Setting
+from ship import Ship
 
 
 def run_game():
@@ -13,6 +14,9 @@ def run_game():
 	screen = pygame.display.set_mode((game_setting.screen_width, game_setting.screen_height))
 	pygame.display.set_caption(game_setting.caption)
 
+	# 创建一个飞船
+	ship = Ship(screen)
+
 	while True:
 	 	# 监视键盘和鼠标事件
 		for event in pygame.event.get():
@@ -21,6 +25,7 @@ def run_game():
 	   			
 		# 每次循环时都重绘屏幕 
 		screen.fill(game_setting.bg_color)
+		ship.blitme()
 
 	 	# 让最近绘制的屏幕可见
 		pygame.display.flip() 
